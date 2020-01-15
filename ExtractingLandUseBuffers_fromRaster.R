cd97a72f2437436341141aa414bbbb73daef81ad
Kévin Barré <kevin.barre@mnhn.fr>
2020-01-15 13:47:17 +0100

    update

--- ExtractingLandUseBuffers_fromRaster.R
+++ ExtractingLandUseBuffers_fromRaster.R
@@ -12,7 +12,7 @@
 # WorkingDirectory = directory at which to save the outpout (ex: "C:/Users/barre/Desktop/")
 
 # Example :
-# extractLandUseRaster=function(dir.posPoints = "C:/Users/barre/Desktop/", 
+# extractLandUseRaster=function(dir.posPoints = "C:/Users/barre/Desktop", 
 #                               name.points = "points", 
 #                               bw = c(50,100,200), 
 #                               id = "inc", 
@@ -33,13 +33,13 @@ extractLandUseRaster=function(dir.posPoints,
       install.packages(new.pkg, dependencies = TRUE)
     sapply(pkg, require, character.only = TRUE)
   } 
-  packages <- c("maptools","sp","raster","raster","lavaan")
+  packages <- c("maptools","sp","raster","rgdal", "data.table","lavaan")
   load(packages)
  
   setwd(WorkingDirectory)
   
   # Opening point shapefile
-  Points<-readOGR(dsn = C:/Users/barre/documents/GitHub/Extracting-land-use-proportion-around-spatial-points-from-environmental-shapefiles, layer = name.points)
+  Points<-readOGR(dsn = dir.posPoints, layer = name.points)
   
   # Converts coordinates into Lambert 93 (2154)
   Points <- spTransform (Points, CRS ("+init=epsg:2154"))
@@ -52,7 +52,7 @@ extractLandUseRaster=function(dir.posPoints,
   
   #plot(Hab)
   
-  HabufPropFinal = data.frame(Points[,ID])
+  HabufPropFinal <<- data.frame(Points[,ID])
   #HabufPropFinal = data.frame(Points[1:10,ID])
   
   for (j in bufwidth) {
@@ -72,17 +72,17 @@ extractLandUseRaster=function(dir.posPoints,
       
       HabufProp[[i]]=as.data.table(t(as.matrix(table(Habuf))/sum(table(Habuf))))
       
-      if(i%%100==1){print(paste("BufferSize",j,Sys.time()))}
+      if(i==nrow(Buffer)){print(paste("BufferSize",j,Sys.time()))}
       
     }
     
-    HabufProp2=rbindlist(HabufProp,fill=T)
+    HabufProp2 = rbindlist(HabufProp,fill=T)
     
     HabufPropTemp = as.data.frame(HabufProp2)
     
-    colnames(HabufPropTemp)=paste0(colnames(HabufPropTemp),"_", j)
+    colnames(HabufPropTemp) = paste0(colnames(HabufPropTemp),"_", j)
     
-    HabufPropFinal=cbind(HabufPropFinal, HabufPropTemp)
+    HabufPropFinal <<- cbind(HabufPropFinal, HabufPropTemp)
     
   }
   
